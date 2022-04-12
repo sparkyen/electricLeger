00c8adace9f923238560958a70332c99b27e659f
sparkyen <1072331197@qq.com>
2022-04-12 21:55:04 +0800

    完善交易的迭代拍卖策略

--- master/application/services/tradeStrategy/main.js
+++ master/application/services/tradeStrategy/main.js
@@ -1,17 +1,20 @@
-function randomNum(m, n){
-    return Math.floor(Math.random()*(n-m))+m;
+function randomNum(m, n) {
+    return Math.floor(Math.random() * (n - m)) + m;
 }
 
-function randomFloat(m, n){
-    return parseFloat((Math.random()*(n-m)+m).toFixed(2));
+function randomFloat(m, n) {
+    return parseFloat((Math.random() * (n - m) + m).toFixed(2));
 }
 
-function dataGenerater(role, num, minPrice, maxPrice, minAmount, maxAmount){
-    let id = 0, data = [];
-    while(id<num){
+function dataGenerater(role, num, minPrice, maxPrice, minAmount, maxAmount) {
+    let id = 0,
+        data = [];
+    while (id < num) {
         let dataDetail = {};
         dataDetail.id = id, id += 1;
-        dataDetail.price = randomFloat(minPrice, maxPrice);
+        dataDetail.expectPrice = randomFloat(minPrice, maxPrice);
+        if (role == 'seller') dataDetail.lowestPrice = randomFloat(minPrice, dataDetail.expectPrice);
+        else dataDetail.highestPrice = randomFloat(dataDetail.expectPrice, maxPrice);
         dataDetail.amount = randomNum(minAmount, maxAmount);
         data.push(dataDetail);
     }
@@ -19,59 +22,94 @@ function dataGenerater(role, num, minPrice, maxPrice, minAmount, maxAmount){
 
 }
 
-function trade(sellBids, buyBids){
-    let m = sellBids.length, n = buyBids.length;
-    let sellRest = [], buyRest = [], tradeRecord = [];
-    let sid = 0, bid = 0;
-    while(sid < m && bid < n){
-        // console.log("sellerId: "+sid+", buyerId: "+bid);
-        let nowSeller = sellBids[sid], nowBuyer = buyBids[bid];
-        if(nowSeller.price<=nowBuyer.price){
-            let tradeDetail = {};
-            tradeDetail.seller = nowSeller.id;
-            tradeDetail.buyer = nowBuyer.id;
-            tradeDetail.price = parseFloat(((nowSeller.price+nowBuyer.price) / 2).toFixed(2));
-            if(nowSeller.amount>=nowBuyer.amount) {
-                tradeDetail.amount = nowBuyer.amount, bid += 1;
-                nowSeller.amount -= nowBuyer.amount, nowBuyer.amount = 0;
+function trade(sellBids, buyBids) {
+    let tradeRecord = [], nowSellRestNum, nowBuyRestNum;
+    var bestPrice = 0, roundNum = 1;
+    while (sellBids.length != 0 && buyBids.length != 0) {
+        let m = sellBids.length, n = buyBids.length;
+        let sid = 0, bid = 0;
+        let sellRest = [], buyRest = [];
+        
+        // trade going
+        while (sid < m && bid < n) {
+            // console.log("sellerId: "+sid+", buyerId: "+bid);
+            let nowSeller = sellBids[sid], nowBuyer = buyBids[bid];
+            if (nowSeller.expectPrice <= nowBuyer.expectPrice) {
+                let tradeDetail = {};
+                tradeDetail.round = roundNum;
+                tradeDetail.seller = nowSeller.id;
+                tradeDetail.buyer = nowBuyer.id;
+                bestPrice = tradeDetail.price = parseFloat(((nowSeller.expectPrice + nowBuyer.expectPrice) / 2).toFixed(2));
+                if (nowSeller.amount >= nowBuyer.amount) {
+                    tradeDetail.amount = nowBuyer.amount, bid += 1;
+                    nowSeller.amount -= nowBuyer.amount, nowBuyer.amount = 0;
+                } else {
+                    tradeDetail.amount = nowSeller.amount, sid += 1;
+                    nowBuyer.amount -= nowSeller.amount, nowSeller.amount = 0;
+                }
+                tradeRecord.push(tradeDetail);
+            } else {
+                sellRest.push(nowSeller), sid += 1;
+                buyRest.push(nowBuyer), bid += 1;
             }
-            
-            else{
-                tradeDetail.amount = nowSeller.amount, sid += 1;
-                nowBuyer.amount -= nowSeller.amount, nowSeller.amount = 0;
-            }
-            tradeRecord.push(tradeDetail);
         }
-        else{
-            sellRest.push(nowSeller), sid += 1;
-            buyRest.push(nowBuyer), bid += 1;
+        console.log('*********************** Round ' + roundNum + ' ***********************');
+
+        if(sellRest.length==sellBids.length && buyRest.length==buyBids.length){
+            console.log("All trade has been done, market closed !")
+            break;
         }
+        console.log('tradeRecord: '), console.log(tradeRecord);
+        console.log('sellRest: '), console.log(sellRest);
+        console.log('buyRest: '), console.log(buyRest);
+
+        console.log('==================== Best Price: '+bestPrice+' ==================== ');
+        //ajust
+        let srid = 0, brid = 0, k = 0.9;
+        for (let i = 0; i < sellRest.length; i++) {
+            if (sellRest[i].expectPrice <= bestPrice) continue;
+            let nowExpectPrice = sellRest[i].expectPrice - k * (sellRest[i].expectPrice - bestPrice);
+            sellRest[i].expectPrice = Math.max(sellRest[i].lowestPrice, parseFloat(nowExpectPrice.toFixed(2)));
+        }
+        for (let i = 0; i < buyRest.length; i++) {
+            if (buyRest[i].expectPrice >= bestPrice) continue;
+            let nowExpectPrice = buyRest[i].expectPrice - k * (buyRest[i].expectPrice - bestPrice);
+            buyRest[i].expectPrice = Math.min(buyRest[i].highestPrice, parseFloat(nowExpectPrice.toFixed(2)));
+        }
+        sellBids = sellRest.sort(function (a, b) {
+            return a.expectPrice - b.expectPrice;
+        });
+        buyBids = buyRest.sort(function (a, b) {
+            return b.expectPrice - a.expectPrice;
+        });
+        roundNum += 1;
+        
+        console.log('After ajust sellBids: '), console.log(sellBids);
+        console.log('After ajust buyBids: '), console.log(buyBids);
+
+        // break;
     }
-    console.log('tradeRecord: '), console.log(tradeRecord);
-    console.log('sellRest: '), console.log(sellRest);
-    console.log('buyRest: '), console.log(buyRest);
+    
+
 
 }
-function generate(){
+
+function generate() {
     var data = [];
-    var sellerData = dataGenerater('seller', 13, 4.5, 9.8, 120, 240).sort(function(a, b){
-        return a.price-b.price;
+    var sellerData = dataGenerater('seller', 13, 4.5, 9.8, 120, 240).sort(function (a, b) {
+        return a.expectPrice - b.expectPrice;
     });
-    var buyerData = dataGenerater('buyer', 9, 2.3, 7.8, 50, 130).sort(function(a, b){
-        return b.price-a.price;
+    var buyerData = dataGenerater('buyer', 9, 2.3, 7.8, 50, 130).sort(function (a, b) {
+        return b.expectPrice - a.expectPrice;
     });
     data.push(sellerData), data.push(buyerData);
     console.log('Seller: '), console.log(sellerData);
     console.log('Buer: '), console.log(buyerData);
     return data;
 }
-var fruits = ["Banana", "Orange", "Apple", "Mango"];
-fruits.sort();
 var data = generate();
 // console.log(data);
 trade(data[0], data[1]);
 
 // console.log(randomNum(7, 34));
 // console.log(randomFloat(2.3, 9.8));
-
-
